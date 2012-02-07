b0c289d5265fb10ef71231ef47e06914d9845056
Brendan Hennessy <bhennes2@gmail.com>
2012-02-07 09:50:12 -0600

    remove omniauth.rb

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-<<<<<<< HEAD
-	provider :twitter, "cSw2k3ueuH0L1bkcQkEAQ", "J1wmhkFjfVyrIRas6rawECeBh5P4N7lnwSw2QVVrw"
-=======
-  provider :twitter, 'cSw2k3ueuH0L1bkcQkEAQ', 'J1wmhkFjfVyrIRas6rawECeBh5P4N7lnwSw2QVVrw' 
->>>>>>> parent of 44b328b... initial
-end
