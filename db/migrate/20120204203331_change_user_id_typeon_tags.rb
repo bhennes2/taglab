37cc2839d51828ea5a5647682334066183063393
Brendan Hennessy <bhennes2@gmail.com>
2012-02-15 10:36:46 -0600

    Change migration for user_id to integer on tags

--- db/migrate/20120204203331_change_user_id_typeon_tags.rb
+++ db/migrate/20120204203331_change_user_id_typeon_tags.rb
@@ -1,6 +1,12 @@
 class ChangeUserIdTypeonTags < ActiveRecord::Migration
   def self.up
-  	change_column :tags, :user_id, :integer
+  	#change_column :tags, :user_id, :integer
+	rename_column :tags, :user_id, :user_id_string
+	add_column :tags, :user_id, :integer
+	
+	Tag.reset_column_information
+	Tag.find_each { |c| c.update_attribute(:user_id, c.user_id_string) }
+	remove_column :tags, :user_id_string
   end
 
   def self.down
