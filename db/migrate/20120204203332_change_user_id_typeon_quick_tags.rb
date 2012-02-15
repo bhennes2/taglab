7fbb29b6dddddc05a70390604170dae5dbbdd09d
Brendan Hennessy <bhennes2@gmail.com>
2012-02-15 10:39:45 -0600

    Change migration for user_id to integer on quicktags

--- db/migrate/20120204203332_change_user_id_typeon_quick_tags.rb
+++ db/migrate/20120204203332_change_user_id_typeon_quick_tags.rb
@@ -1,6 +1,13 @@
 class ChangeUserIdTypeonQuickTags < ActiveRecord::Migration
   def self.up
-  	change_column :quick_tags, :user_id, :integer
+  	#change_column :quick_tags, :user_id, :integer
+		
+	rename_column :quick_tags, :user_id, :user_id_string
+	add_column :quick_tags, :user_id, :integer
+	
+	QuickTag.reset_column_information
+	QuickTag.find_each { |c| c.update_attribute(:user_id, c.user_id_string) }
+	remove_column :quick_tags, :user_id_string
   end
 
   def self.down
