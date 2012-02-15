2b0eb5584749c600771a2700df23fd5f6a1a786f
Brendan Hennessy <bhennes2@gmail.com>
2012-02-15 10:12:37 -0600

    Fix migration for age change to type date

--- db/migrate/20120204203328_change_age_typefor_user.rb
+++ db/migrate/20120204203328_change_age_typefor_user.rb
@@ -1,6 +1,12 @@
 class ChangeAgeTypeforUser < ActiveRecord::Migration
   def self.up
-  	change_column :users, :age, :date
+  	#change_column :users, :age, :date
+	rename_column :users, :age, :age_string
+	add_column :users, :age, :date
+	
+	User.reset_column_information
+	User.find_each { |c| c.update_attribute(:age, c.age_string) }
+	remove_column :users, :age_string
   end
 
   def self.down
