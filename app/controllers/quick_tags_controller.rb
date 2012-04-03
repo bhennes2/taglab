ff409ae8d4e40b09806e043b3de5920742f203de
Brendan Hennessy <bhennes2@gmail.com>
2012-04-03 15:48:40 -0500

    Change quicktag query

--- app/controllers/quick_tags_controller.rb
+++ app/controllers/quick_tags_controller.rb
@@ -3,7 +3,7 @@ class QuickTagsController < ApplicationController
   # GET /quick_tags.xml
   def index
   	@title = "Quick tags"
-    @quick_tags = QuickTag.where(:user_id => current_user.id).reorder("category DESC").group("category")
+    @quick_tags = QuickTag.where(:user_id => current_user.id).order("category DESC")
 
     respond_to do |format|
       format.html # index.html.erb
